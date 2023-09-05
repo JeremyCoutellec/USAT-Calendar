c84669957891e29fd90175bc487fd590e2361979
Jeremy Coutellec <jeremy.coutellec@umake.fr>
2023-09-05 10:59:30 +0200

    feat: add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:usat_calendar/src/locator.dart';
 
+import 'firebase_options.dart';
 import 'src/app.dart';
 
 Future main() async {
-  await initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
