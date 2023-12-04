92aec76bb019fa3116ae6c29539e69d5d48219af
Jeremy Coutellec <j.coutellec@pyres.com>
2023-12-04 12:15:12 +0100

    init: pubspec filter

--- linux/main.cc
+++
@@ -1,6 +0,0 @@
-#include "usat_calendarlication.h"
-
-int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = usat_calendarlication_new();
-  return g_application_run(G_APPLICATION(app), argc, argv);
-}
